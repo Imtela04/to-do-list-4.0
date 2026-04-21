55416a4423fd11414d5b958e2f958e425687d0bc
Imtela04 <imtela@gmail.com>
2026-04-21 18:35:26 +0600

    added cors middleware

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'apps.todo.apps.TodoConfig',
     'apps.accounts.apps.AccountsConfig',
     'django.contrib.admin',
@@ -42,7 +43,14 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    # "https://imtela04.github.io",
+    "http://localhost:5173",
+    "http://localhost:3000",
+]
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,6 +130,10 @@ TIME_ZONE = 'Asia/Dhaka'
 USE_I18N = True
 
 USE_TZ = True
+
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_CREDENTIALS = True
+CORS_URLS_REGEX = r'^.*$'
 
 
 # Static files (CSS, JavaScript, Images)
