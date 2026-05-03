076001c5a914aef02c849d04ee455c956946fe7d
Imtela04 <imtela@gmail.com>
2026-05-03 18:44:52 +0600

    added query hooks

--- frontend/src/hooks/useTasksQuery.js
+++ frontend/src/hooks/useTasksQuery.js
@@ -1,13 +1,13 @@
-// src/hooks/useTasksQuery.js
 import { useQuery } from '@tanstack/react-query';
+import { getTasks } from '@/api/services';
 
-export function useTasksQuery() {
+export function useTasksQuery(enabled = !!localStorage.getItem('authToken')) {
   return useQuery({
     queryKey: ['tasks'],
     queryFn: async () => {
       const res = await getTasks();
       return res.data.results || res.data;
     },
-    enabled: !!localStorage.getItem('authToken'),
+    enabled,
   });
 }
