6d70d55b47ad1f9183af5627fb9b19c81fe881a6
Imtela04 <imtela@gmail.com>
2026-06-24 14:58:30 +0600

    fixes frontend/backend separation

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/static/' : '/',
+  base: process.env.NODE_ENV === '/',
   build: {
     emptyOutDir: true,
   },
