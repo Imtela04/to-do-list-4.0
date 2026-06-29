0b1e3ab6c13a709b24e0c4d62e9353c64485c3da
Imtela04 <imtela@gmail.com>
2026-06-29 16:37:33 +0600

    converting to pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path'
 
 export default defineConfig({
-
-  plugins: [react()],
-  resolve: {
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // Automatically updates the app when you deploy new changes
+      injectRegister: 'auto', // Automatically registers the Service Worker in your index.html
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'], // Add your static assets here
+      manifest: {
+        name: 'What Do?',
+        short_name: 'What Do',
+        description: 'An all-in-one productivity app: slack no more.',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/logo192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ],
+      },
+    }),
+  ],
+    resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
